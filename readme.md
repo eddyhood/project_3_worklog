a2c275e0bcaebba3e3100989c683ade74f941d56
Eddy Hood <eddy@ignitespot.com>
2017-07-11 09:55:11 -0600

    Added instructions and further readme info

--- readme.md
+++ readme.md
@@ -3,3 +3,11 @@
 Worklog is a basic commandline application for entering, tracking, and
 reporting on timestamped entires within a company.
 
+The goal of this project is to help devlop skills in the following areas:
+1.  Regular expressions
+2.  Datetime & timezone applications
+3.  Object oriented programming
+4.  I/O management with a CSV file
+4.  PEP 8 styling
+
+This work was submitted by Eddy Hood in July 2017.
---
+++ worklog.py
@@ -0,0 +1,38 @@
+# Present a menu of options: Add new entry or look up previous entry
+
+# Runs without error and all exceptions are caught in a meaningful way
+
+# When entering a new work log, user enters: task name, date,
+# number of minutes spent, and any additional notes they want to record.
+
+# When finding previous entry, they can: find by date, find by time spent,
+# find by exact search, find by pattern
+
+# When finding by date, present a list of dates with entries and be able
+# to choose on to see entries from.
+
+# When finding by time spent, user can enter the number of minutes a task took
+# and be able to choose one to see entires from.
+
+# When finding by exact string, user should be allowed to enter a string and
+# then be presented with entries containing that string in the task name
+# or notes.
+
+# When finding by pattern, user should be allowed to enter a regular expression
+# and then be presented with entries matching that pattern in their task
+# name or notes.
+
+# When displaying the entries, the entires should be in a readable format with
+# the date, task name, time spent, and note info.
+
+# Menu must have a quit option to exit the program.
+
+# Entries can be deleted and edited, letting user change the date, task name,
+# time spent, and/or notes.
+
+# Entries can be searched for and found based in a date range. For example,
+# 01/01/2016 and 12/31/2016.
+
+# Entries are displayed one at a time with the ability to page through
+# records (previous,next/back).
+
